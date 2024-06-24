58e50e08d4da00326c9075dd5a18adcd2b317d72
tmdwnsasa <tmdwns_as@naver.com>
2024-06-24 10:10:40 +0900

    [level 2] Title: n^2 배열 자르기, Time: 9.12 ms, Memory: 44 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ JavaScript/프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 43.5 MB, 시간: 4.80 ms
+메모리: 44 MB, 시간: 9.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 24일 10:04:18
+2024년 06월 24일 10:10:38
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ JavaScript/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,31 +1,8 @@
 function solution(n, left, right) {
     var answer = [];
-    for(let i = Math.floor(left / n); i <= Math.floor(right/n); i++){
-        let j, max;
-        if (Math.floor(left/n) === Math.floor(right/n)){
-            j = left%n;
-            max = right%n;
-        }
-        else if(i === Math.floor(left/n)){
-            j = left%n;
-            max = n-1;
-        }
-        else if (i === Math.floor(right/n)){
-            j = 0;
-            max = right%n;
-        }
-        else{
-            j = 0;
-            max = n-1;
-        }
-        for(; j <= max; j++){
-            if(i > j){
-                answer.push(i+1);
-            }
-            else{
-                answer.push(j+1);
-            }
-        }
+    for(let i = left; i <= right; i++){
+        answer.push(Math.max(Math.floor(i / n)+1, (i % n)+1))
     }
+    console.log(answer);
     return answer;
 }
